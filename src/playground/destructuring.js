e8c6f84949d42264303e6b77518f63b59044f814
Awilda I Couverthier <awildacouverthier@gmail.com>
2020-04-01 22:47:27 -0400

    ES6 object destructuring

---
+++ src/playground/destructuring.js
@@ -0,0 +1,39 @@
+// const person = {
+// 	name: 'Awilda',
+// 	age: 30,
+// 	location: {
+// 		city: 'New York City',
+// 		temp: 49
+// 	}
+// };
+
+// // const name = person.name;
+// // const age = person.age;
+// const { name, age } = person;
+// console.log(`${name} is ${age}`);
+
+
+// // Using default value if no value is provided
+// const { name = 'Anonymous', age } = person;
+// console.log(`${name} is ${age}`);
+
+
+// // const { city, temp } = person.location;
+// // console.log(`It's ${temp} degrees in ${city}`);
+
+// // renaming a variable
+// const { city, temp: temperature } = person.location;
+// console.log(`It's ${temperature} degrees in ${city}`);
+
+
+const book = {
+	title: 'The Starless Sea',
+	author: 'Erin Morgenstern',
+	publisher: {
+		name: 'Doubleday'
+	}
+};
+
+const { name: publisherName = 'Self-Published' } = book.publisher;
+
+console.log(publisherName); // if no valid name, use Self-Published
